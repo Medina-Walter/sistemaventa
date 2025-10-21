129a353d24d8759436f4d71798b09bac87068f3e
Camila Ozuna <camiozuna98@gmail.com>
2025-10-21 01:23:58 -0300

    feat: agrego modelos iniciales

---
+++ app/Models/Producto.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'productos';
+    protected $primaryKey = 'id_producto';
+
+    protected $fillable = [
+        'nombre',
+        'codigo',
+        'id_imagen',
+        'descripcion',
+        'stock',
+        'precio_compra',
+        'precio_venta',
+        'id_categoria',
+        'id_proveedor',
+    ];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'id_categoria');
+    }
+
+    public function proveedor()
+    {
+        return $this->belongsTo(Proveedor::class, 'id_proveedor');
+    }
+
+    public function imagen()
+    {
+        return $this->belongsTo(Imagen::class, 'id_imagen');
+    }
+
+    public function detallesVenta()
+    {
+        return $this->hasMany(DetalleVenta::class, 'id_producto');
+    }
+}
