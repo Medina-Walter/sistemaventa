7be2ba69f0cc9c89e558fa157e7515d97d28cf9b
Walter <waltermedina1357@gmail.com>
2025-10-25 23:27:14 -0300

    añadir logica de login de usuarios

---
+++ app/Models/Usuario.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id_usuario';
+
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'usuario',
+        'correo',
+        'clave',
+        'rol',
+    ];
+
+    public function ventas()
+    {
+        return $this->hasMany(Venta::class, 'id_usuario');
+    }
+}
---
+++ database/migrations/2025_10_17_175819_create_productos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+    Schema::create('productos', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->decimal('precio', 8, 2);
+        $table->integer('stock');
+
+        // Si el producto tiene imagen opcional:
+        $table->unsignedBigInteger('id_imagen')->nullable();
+
+        $table->foreign('id_imagen')
+              ->references('id')
+              ->on('imagenes')
+              ->onDelete('set null');
+
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
